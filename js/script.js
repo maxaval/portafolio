ac1d680c8fe80b905e398ca89a2758ea081ef4c1
Maximiliano Avalos <maxaval@gmail.com>
2024-09-13 22:36:09 -0600

    Portafolio v1.0

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-const imagen = document.querySelector('.efecto-imagen');
-const mask = document.querySelector('.mask');
+/*const banner = document.querySelector('.banner');
+const textSections = document.querySelectorAll('.text-section');
 
-imagen.addEventListener('mousemove', (e) => {
-  const rect = imagen.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const grayscaleAmount = x / rect.width;
-  imagen.style.filter = `grayscale(${grayscaleAmount})`;
-});
+banner.addEventListener('mousemove', (e) => {
+  const x = e.clientX;
+  const width = banner.offsetWidth;
+  const percentage = (x / width) * 100;
 
-imagen.addEventListener('mouseout', () => {
-  imagen.style.filter = 'grayscale(0)';
-});
+  banner.style.filter = 'grayscale(${100 - percentage}%)';
+  textSections.forEach((textSection, index) => {
+    if (index === 0) {
+      textSection.style.opacity = 1 - (percentage / 100);
+    } else {
+      textSection.style.opacity = percentage / 100;
+    }
+  });
+});*/
